1056d0cd8fceece429f00448c8484d630eaca0f1
Jaylen Sandoval <jaylensandoval@Jaylens-Air>
2023-06-15 16:42:10 -0700

    changes made to block 24, css, data, and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
-
-import './App.css'
+import "./App.css";
+import { puppyList } from "./data.js";
+import { useState } from "react";
+import "./index.css";
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
+  function handleClick() {
+    {
+      featuPupID && <p>{featPupId}</p>;
+    }
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
-    <>
-      <div>
-        
-      </div>
-      
-    </>
-  )
+    <div className="App">
+      {featPupId && (
+        <div className="selectedPupper">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+
